9265af405cd76647d650f24e70f7f57702b60028
Tasos Nikolaou <anikolaou@uphellas.gr>
2026-03-03 12:38:04 +0200

    [PULSE] Lazy imports update

--- pulsecheck/core/checks/__init__.py
+++ pulsecheck/core/checks/__init__.py
@@ -18,3 +18,34 @@ __all__ = [
     "CeleryInspectCheck",
     "HttpDependencyCheck",
 ]
+
+def __getattr__(name: str):
+    if name == "SQLAlchemyAsyncCheck":
+        from .sqlalchemy_async import SQLAlchemyAsyncCheck
+        return SQLAlchemyAsyncCheck
+
+    if name == "DjangoDBCheck":
+        from .django_db import DjangoDBCheck
+        return DjangoDBCheck
+
+    if name == "RedisAsyncCheck":
+        from .redis_async import RedisAsyncCheck
+        return RedisAsyncCheck
+
+    if name == "RedisSyncCheck":
+        from .redis_sync import RedisSyncCheck
+        return RedisSyncCheck
+
+    if name == "RabbitMQKombuCheck":
+        from .rabbitmq_kombu import RabbitMQKombuCheck
+        return RabbitMQKombuCheck
+
+    if name == "CeleryInspectCheck":
+        from .celery_inspect import CeleryInspectCheck
+        return CeleryInspectCheck
+
+    if name == "HttpDependencyCheck":
+        from .http_dep import HttpDependencyCheck
+        return HttpDependencyCheck
+
+    raise AttributeError(f"module {__name__} has no attribute {name}")
